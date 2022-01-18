a949ee756cdbc014dbba6f3295d827755f75d895
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-01-18 11:35:05 -0300

    aula05

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,8 @@ export default function PaginaInicial() {
             onSubmit={function (infosDoEvento) {
               infosDoEvento.preventDefault();
               console.log('Alguém submeteu o form');
-              roteamento.push('/chat');
+              // roteamento.push('/chat?username=' + username);
+              roteamento.push(`/chat?username=${username}`);
               // window.location.href = '/chat';
             }}
             styleSheet={{
